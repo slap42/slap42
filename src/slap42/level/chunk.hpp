c4d9018ae1e3719c40a9ea0bd8980f11d92d4263
Jack <slap42game@gmail.com>
2024-08-14 16:17:54 +0100

    macOS update

--- src/slap42/level/chunk.hpp
+++ src/slap42/level/chunk.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstddef>
 #include "graphics/mesh.hpp"
 
 namespace Slap42 {
--- src/slap42/main.cpp
+++ src/slap42/main.cpp
@@ -31,6 +31,11 @@ int main() {
   
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
+
   GLFWwindow* window = glfwCreateWindow(1280, 720, "Test", nullptr, nullptr);
   if (!window) {
     fprintf(stderr, "glfwCreateWindow failed\n");
@@ -54,7 +59,9 @@ int main() {
 
   Camera camera(window, level.GetShader());
   glfwSetWindowUserPointer(window, &camera);
-  auto on_resize = [](GLFWwindow* window, int width, int height) {
+  auto on_resize = [](GLFWwindow* window, int w, int h) {
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
     Camera* camera = (Camera*)glfwGetWindowUserPointer(window);
     camera->OnResize(width, height);
     glViewport(0, 0, width, height);
